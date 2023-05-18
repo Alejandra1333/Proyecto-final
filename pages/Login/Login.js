7c7c1be0da5200c6ec9dc2c02da51e37e3bbf934
Alejandra1333 <129126543+Alejandra1333@users.noreply.github.com>
2023-05-17 23:17:21 -0500

    Add files via upload

---
+++ pages/Login/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Title from "./components/Title/title";
+
+const Login = () => {
+    return(
+        <div>
+            <Title text='Soy un titulo' />
+        </div>
+    )
+};
+export default Login;
---
+++ pages/Login/components/Title/Title.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Title = (props) => {
+    return(
+        <div>
+           (props.text)
+        </div>
+    )
+};
+export default Title;
